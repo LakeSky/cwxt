bbd201ecac3b04786f7d203c4b63a51129759905
LakeSky <623907705@qq.com>
2013-12-22 22:52:00 +0800

    definePanel

---
+++ src/main/java/com/kzh/busi/employee/action/EmployeeAction.java
@@ -0,0 +1,13 @@
+package com.kzh.busi.employee.action;
+
+import com.kzh.util.struts.BaseAction;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kzh
+ * Date: 13-12-22
+ * Time: 下午10:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmployeeAction extends BaseAction {
+}
---
+++ src/main/java/com/kzh/busi/employee/dao/EmployeeDao.java
@@ -0,0 +1,13 @@
+package com.kzh.busi.employee.dao;
+
+import com.kzh.util.hibernate.BaseDao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kzh
+ * Date: 13-12-22
+ * Time: 下午10:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmployeeDao extends BaseDao {
+}
